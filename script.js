ab9429a9745193e41db1023dd0d54e968e5c223a
GDAcourse <sandyeditor37@gmail.com>
2026-01-30 15:44:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 function login() {
-  var user = document.getElementById("username").value;
-  var pass = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-  var url = "https://script.google.com/macros/s/AKfycbz8JmVVn4pTwp59c2tFGSFhhB4VIjtaGcA-dnTc4uxm76C1IITaavSxwNTz1Yo0sGgF/exec"
-    + "?username=" + user
-    + "&password=" + pass;
+  fetch("https://script.google.com/macros/s/AKfycbzzoNBQXcYQ-r412WHOTZpw_qw0dudQwMFH2pbQLtonnfL71iSb6nQbnRDz4_ArY05O/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
 
-  fetch(url)
-    .then(res => res.text())
-    .then(result => {
-      if (result.trim() === "success") {
-        window.location.href = "dashboard.html";
-      } else {
-        alert("Wrong Username or Password");
-      }
-    });
+    if (data.status === "success") {
+      window.location.href = "dashboard.html";
+    }
+    else if (data.status === "inactive") {
+      alert("Your account is inactive due to 15 days inactivity. Please contact institute.");
+    }
+    else {
+      alert("Invalid username or password");
+    }
+
+  });
 }
